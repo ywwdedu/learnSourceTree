17713a7d6792e527ef9446d08cc947f2609291d4
w_yu <w_yu@Ctrip.com>
2018-12-04 17:11:01 +0800

    all project.

---
+++ learnSourceTree/src/main/java/com/yuwei/learnSourceTree/LearnSourceTreeApplication.java
@@ -0,0 +1,12 @@
+package com.yuwei.learnSourceTree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSourceTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSourceTreeApplication.class, args);
+	}
+}
